1de3ea74db49a134497e4d10853778104d198fd8
flrivera <flrivera@uci.edu>
2022-01-07 22:19:18 -0500

    Update Recog_app.py

--- Recog_app.py
+++ Recog_app.py
@@ -163,7 +163,35 @@ def img2np( filename):
     return Im_correct_dimensions
 
 
-#Get feature of image
+#Get footer
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' target="_blank">Freida L. Rivera Gareriga [TDI-Capstone] S</a></p>
+</div>
+"""
+
 
 
 
@@ -281,7 +309,7 @@ else:
 
 
 
-    st.footer('Freida Rivera','TDI-Capstone')
+    st.markdown(footer,unsafe_allow_html=True)
    
 
 
